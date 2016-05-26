571bdbabe28b082c61848894a954b2dcd4e0beae
weiguangqiang <weiguangqiang820@gmail.com>
2016-05-26 10:07:42 +0800

    Upload new project of ShareFileServer

---
+++ ShareFileServer/app/src/main/java/wei/example/com/sharefileserver/MainActivity.java
@@ -0,0 +1,13 @@
+package wei.example.com.sharefileserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
